2c8bc9627f2c9e02ef0f9c946aa9b1321ccae8da
xuantruong2004 <truongxuantp3@gmail.com>
2022-08-29 21:01:21 +0700

    Remove app.css

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+// import { engine } from "express-handlebars";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "/resources/views"));
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
